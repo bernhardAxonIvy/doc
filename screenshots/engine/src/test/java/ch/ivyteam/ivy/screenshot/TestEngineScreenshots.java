baac80ed4bf1eb8b0e504855f7528b02004366af
Lukas Lieb <lukas.lieb@axonivy.com>
2022-04-11 08:17:57 +0200

    Fix engine screenshot doc build

--- screenshots/engine/src/test/java/ch/ivyteam/ivy/screenshot/TestEngineScreenshots.java
+++ screenshots/engine/src/test/java/ch/ivyteam/ivy/screenshot/TestEngineScreenshots.java
@@ -124,7 +124,7 @@ public class TestEngineScreenshots {
 
   private void defineGroupAssingeeForProcessStep() {
     PrimeUi.selectOneRadio(By.id("assignee-selection-form:assignee-type")).selectItemByValue("Group");
-    $(By.id("assignee-selection-form:role-selection-component:role-selection_input")).shouldBe(visible).sendKeys("Everybody");
+    $(By.id("assignee-selection-form:role-selection-component:role-selection")).find(".ui-icon-triangle-1-s").shouldBe(visible).click();
     $(By.id("assignee-selection-form:role-selection-component:role-selection_panel")).shouldBe(visible)
             .find("ul li[data-item-label='Everybody']").click();
     addAssignee("Everybody");
