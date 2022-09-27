285279fdc7591553a356c57dea7372abec24a4f4
Peter Hochstrasser <peter.hochstrasser@axonivy.com>
2022-09-27 16:22:35 +0200

    XIVY-9588 doc build container: start Firefox only if present (#794)

--- up.sh
+++ up.sh
@@ -2,5 +2,10 @@
 
 rm -rf build
 mvn clean package
-(sleep 20 && firefox http://localhost:8000) &
+
+# only start firefox if it is available.
+which firefox > /dev/null
+if [ $? == 0 ]; then
+   (sleep 20 && firefox http://localhost:8000) &
+fi
 docker-compose up
