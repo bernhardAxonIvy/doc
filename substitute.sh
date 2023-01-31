1d38cf94f29027bd82dabb15dc287ff4052c083f
Peter Hochstrasser <peter.hochstrasser@axonivy.com>
2023-01-31 16:10:50 +0100

    XIVY-10646 doc added .dot file type for substitution as well (#1890)
    * XIVY-10646 doc added .dot file type for substitution as well
    
    * XIVY-10646 added console output to substitute.sh
    
    * XIVY-10646 more debug output
    
    * XIVY-10646 rewrite into one loop per file type
    
    * XIVY-10646 removed some debug code
    
    * XIVY-10646 additional beautification
    
    * XIVY-10646 pattern needed to be enclosed in double quotes

--- substitute.sh
+++ substitute.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-version="$1"            # 10.0.0 etc
-major="${version%%.*}"  # 10
+version="$1"            # e.g. 10.0.0
+major="${version%%.*}"  # e.g. 10
 
-# switch to directory of script
-pushd $(dirname "${BASH_SOURCE[0]}") 2>&1 1> /dev/null
+placeholderstarts="\|(ivy|version|major)" # start of placeholder strings
 
-cd source/
-
-# check all RST files for start of placeholders, and feed them to sed
-# execute sed only on files containing placeholders.
-for name in $(find . -name "*.rst" -exec grep -lE "\|(ivy|version|major)" {} \;) ; do
+replace_placeholders() {
+    name=$1
+    echo "substituting $name"
     sed -i -E "s/\|ivy-designer\|/Axon Ivy Designer/g; \
             s/\|ivy-engine\|/Axon Ivy Engine/g; \
             s/\|ivy\|/Axon Ivy/g; \
             s/\|version\|/$version/g; \
             s/\|major-version\|/$major/g;" $name
-done
+    cat $name | grep -E $placeholderstarts
+}
+
+find_and_replace_placeholders() {
+    pattern=$1
+    for name in $(find . -type f -name "$pattern" -exec grep -lE $placeholderstarts {} \;) ; do
+        # echo $name
+        replace_placeholders $name
+    done
+}
+
+# switch to directory of script
+pushd $(dirname "${BASH_SOURCE[0]}") 2>&1 1> /dev/null
+
+echo "$0 started in $(pwd)"
+cd source/
+
+find_and_replace_placeholders "*.rst"
+find_and_replace_placeholders "*.dot"
 
 popd 2>&1 1> /dev/null
+
+echo "$0 complete."
+
