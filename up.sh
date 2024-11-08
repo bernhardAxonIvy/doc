b97f1c6f79fccb7004ff5997023589a817b6b86a
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-11-08 16:29:51 +0100

    responsive up-script: start firefox as soon as the HTML files are being served

--- up.sh
+++ up.sh
@@ -3,11 +3,21 @@
 rm -rf build
 mvn clean package
 
-# only start firefox if it is available.
-which firefox > /dev/null
-if [ $? == 0 ]; then
-   (sleep 20 && firefox http://localhost:8000) &
-fi
-
 export UID=$(id -u)
+
+browseDoc(){
+  # only start firefox if it is available.
+  if [ -x "$(command -v firefox)" ]; then
+    while [ -z "${started}" ]
+    do
+      sleep 1
+      echo "[browser] waiting for HTTP service"
+      started=$(docker compose logs --since 1m | grep "HTML pages")
+    done
+    firefox http://localhost:8000 &
+  fi
+}
+
+browseDoc & 
+
 docker compose up
